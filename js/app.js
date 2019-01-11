8e90ca9a35f7f183a5274ba70c9f088e4f710d80
Kishan <kkpatel911@gmail.com>
2019-01-11 11:31:57 -0500

    added functionalty to options

--- js/app.js
+++ js/app.js
@@ -5,21 +5,27 @@ const form = document.querySelector('form');
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-fetch('https://dog.ceo/api/breeds/image/random')
-  .then(function (response) {
-    return response.json();
-  })
+
+
+
+function fetchData(url) {
+  return fetch(url)
+    .then(function (res) {
+      return res.json();
+    })
+}
+
+
+
+fetchData('https://dog.ceo/api/breeds/image/random')
   .then(function (data) {
     generateMessage(data.message);
   });
 
-fetch('https://dog.ceo/api/breeds/list/all')
-  .then(function (response) {
-    return response.json();
-  })
+fetchData('https://dog.ceo/api/breeds/list/all')
   .then(function (data) {
     generateOptions(Object.keys(data.message));
-  })
+  });
 
 
 // ------------------------------------------
@@ -28,21 +34,35 @@ fetch('https://dog.ceo/api/breeds/list/all')
 
 function generateMessage(data) {
   var html = "<img src = " + data + ">";
-  html += "<p>Click To View images of " + select.value + "</p>s"
+  html += "<p>Click To View images of " + select.value + "</p>"
   card.innerHTML = html;
 }
 
 function generateOptions(data) {
-  let output = '';
+  var output = "";
   data.forEach(function (key) {
-    output += `<option>${(key)}</option>`});
-    console.log(output);
+    output += `<option>${(key)}</option>`
+  });
+  select.innerHTML = output;
 }
 
+
+function fetchBreedImage() {
+  var breed = select.value;
+  var img = card.querySelector('img');
+  var p = card.querySelector('p');
+  fetchData(`https://dog.ceo/api/breed/${breed}/images/random`) 
+  .then(function(data){
+    img.src = data.message;
+    img.alt = breed;
+    p.textContent = `Click to View more ${breed}`;
+  })
+}
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
-
+select.addEventListener('change', fetchBreedImage);
+card.addEventListener('click', fetchBreedImage);
 
 
 // ------------------------------------------
